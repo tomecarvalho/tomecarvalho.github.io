9e68853e68449e6f2931ab9e4b4b30adf346128d
Tomé Carvalho <tomecarvalho01@gmail.com>
2026-06-13 12:25:47 +0100

    Nav icons and tweaks to look more like adwaita apps

--- src/routes/nav-routes.ts
+++ src/routes/nav-routes.ts
@@ -4,13 +4,14 @@ import type { Overwrite } from "../types/util/Overwrite";
 export interface NavRoute {
   label: LocaleString;
   path: string;
+  icon?: string;
 }
 
 export type TranslatedNavRoute = Overwrite<NavRoute, { label: string }>;
 
 export const navRoutes = {
-  aboutMe: { label: "nav.aboutMe", path: "/" },
-  projects: { label: "nav.projects", path: "/projects" },
+  aboutMe: { label: "nav.aboutMe", path: "/", icon: "mdi:home" },
+  projects: { label: "nav.projects", path: "/projects", icon: "mdi:folder" },
 } as const satisfies Record<string, NavRoute>;
 
 /** `navRoutes` entries ordered by the order they should appear in the nav component. */
