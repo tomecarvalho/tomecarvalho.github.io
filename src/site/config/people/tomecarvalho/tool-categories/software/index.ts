502b99ee96749d42db47157b72e514f5dcb13b0c
Tomé Carvalho <tomecarvalho01@gmail.com>
2026-06-28 13:39:19 +0100

    Add LocalSend to tools

--- src/site/config/people/tomecarvalho/tool-categories/software/index.ts
+++ src/site/config/people/tomecarvalho/tool-categories/software/index.ts
@@ -1,11 +1,12 @@
 import type { ToolCategory } from "../../../../../../types/data/tools/ToolCategory";
 import linux from "./tools/linux";
+import localSend from "./tools/local-send";
 
 const software = {
   id: "software",
   name: { en: "Software", pt: "Software" },
   icon: "mdi:software",
-  tools: [linux],
+  tools: [linux, localSend],
 } as const satisfies ToolCategory;
 
 export default software;
