8f495b7e27e8a72873a902bd0df03641316ef6c4
Tomé Carvalho <tomecarv@gmail.com>
2026-08-23 21:53:42 +0200

    Replace The Witcher books by Project Hail Mary

--- src/site/config/people/tomecarvalho/interests/reading/index.ts
+++ src/site/config/people/tomecarvalho/interests/reading/index.ts
@@ -1,11 +1,11 @@
 import type { Interest } from "../../../../../../types/data/interests/Interest";
-import { Content as DescriptionEn } from "./description.en.md";
-import { Content as DescriptionPt } from "./description.pt.md";
-import theCosmere from "./books/the-cosmere";
 import lonesomeDove from "./books/lonesome-dove";
+import projectHailMary from "./books/project-hail-mary";
+import theCosmere from "./books/the-cosmere";
 import theCountOfMonteCristo from "./books/the-count-of-monte-cristo";
 import theDreamMachine from "./books/the-dream-machine";
-import theWitcher from "./books/the-witcher";
+import { Content as DescriptionEn } from "./description.en.md";
+import { Content as DescriptionPt } from "./description.pt.md";
 
 const reading = {
   id: "reading",
@@ -24,7 +24,7 @@ const reading = {
   ],
   cards: [
     theCosmere,
-    theWitcher,
+    projectHailMary,
     theCountOfMonteCristo,
     lonesomeDove,
     theDreamMachine,
